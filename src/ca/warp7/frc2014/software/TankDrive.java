14b5255ce517b8b4736933e588ecc4ce8e922ebc
aerobit <arilotter@gmail.com>
2014-01-09 15:07:12 -0500

    Added blank tank drive class

---
+++ src/ca/warp7/frc2014/software/TankDrive.java
@@ -0,0 +1,15 @@
+package ca.warp7.frc2014.software;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ari
+ * Date: 1/9/14
+ * Time: 3:04 PM
+ */
+public class TankDrive extends Subsystem {
+
+    public void tick() {
+        // Blah blah
+
+    }
+}
