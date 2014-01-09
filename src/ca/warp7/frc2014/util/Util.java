ca011c5048d008dfc6df29c3901c1c7c31d4a6d9
aerobit <arilotter@gmail.com>
2014-01-09 15:32:13 -0500

    Added some util stuff for future use once we get our drivetrain & stuff planned out

--- src/ca/warp7/frc2014/util/Util.java
+++ src/ca/warp7/frc2014/util/Util.java
@@ -7,4 +7,24 @@ package ca.warp7.frc2014.util;
  * Time: 3:22 PM
  */
 public class Util {
+    private Util() {
+        // Stop an instance of this class from being created
+    }
+
+    /**
+     * Stolen from
+     * https://github.com/Team254/FRC-2013/blob/master/src/com/team254
+     * /lib/util/Util.java
+     *
+     * @author richard@team254.com (Richard Lin)
+     * @author brandon.gonzalez.451@gmail.com (Brandon Gonzalez) Thanks to the
+     * cheesy poofs! Limits the given input to the given magnitude.
+     */
+    public static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1); //This is used for cheesydrive, if we're gonna use it.
+    }
+
+    public static void log(String string) {
+        System.out.println(string);
+    }
 }
