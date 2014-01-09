d55c4c54fa94c765d9729c8016c3a7b2d24db15d
aerobit <arilotter@gmail.com>
2014-01-09 15:36:34 -0500

    Fixed credits

--- src/ca/warp7/frc2014/util/Util.java
+++ src/ca/warp7/frc2014/util/Util.java
@@ -15,10 +15,6 @@ public class Util {
      * Stolen from
      * https://github.com/Team254/FRC-2013/blob/master/src/com/team254
      * /lib/util/Util.java
-     *
-     * @author richard@team254.com (Richard Lin)
-     * @author brandon.gonzalez.451@gmail.com (Brandon Gonzalez) Thanks to the
-     * cheesy poofs! Limits the given input to the given magnitude.
      */
     public static double limit(double v, double limit) {
         return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1); //This is used for cheesydrive, if we're gonna use it.
