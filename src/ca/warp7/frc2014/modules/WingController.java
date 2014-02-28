ff853912f71251c1d8b3912b4b9030d1c74624be
Ari Lotter <arilotter@gmail.com>
2014-02-28 09:26:43 -0500

    Expressing my feelings

--- src/ca/warp7/frc2014/modules/WingController.java
+++ src/ca/warp7/frc2014/modules/WingController.java
@@ -18,116 +18,127 @@ public class WingController extends ModuleBase {
 
     public static WingController getInstance() {
         return instance;
-    }
+    } // whoo hooray for the singleton pattern
 
     public void periodic() {
-        setState(robot.ds.controller.getModeButton());
+        int butt = robot.ds.controller.getModeButton();
+        if (butt != -1) {
+            setState(butt);
+        }
+
         if (timer > 0) {
             timer--;
         }
+
         if (timer == 0) {
             switch (STATE) {
                 case WingModes.DROP:
                     setState(WingModes.DROP_KICK);
                     break;
-
+                case WingModes.DROP_KICK:
+                    setState(WingModes.SHOULD_FLUSH); //Reset to neutral
+                    break;
                 case WingModes.SHOULD_FLUSH:
                     setState(WingModes.FLUSH);
                     break;
             }
+
             timer = -1;
         }
     }
 
     public void setState(int state) {
-        if (state != -1) {
-            STATE = state;
-            robot.ds.table.putNumber("wingMode", STATE);
+        STATE = state;
+        robot.ds.table.putNumber("wingMode", STATE);
 
-            switch (state) {
-                case WingModes.CATCH:
-                    // Rollers go up, arms go up. \[]/
-                    Util.log("WingController", "Mode: CATCH");
+        switch (state) {
+            case WingModes.CATCH:
+                // Rollers go up, arms go up. \[]/
+                Util.log("WingController", "Mode: CATCH");
 
-                    robot.hw.backWing.rollersUp();
-                    robot.hw.frontWing.rollersUp();
+                robot.hw.backWing.rollersUp();
+                robot.hw.frontWing.rollersUp();
 
-                    robot.hw.frontWing.setTargetAngle(135); // guesstimated #.
-                    robot.hw.backWing.setTargetAngle(135);
+                robot.hw.frontWing.setTargetAngle(135); // guesstimated #.
+                robot.hw.backWing.setTargetAngle(135);
+                break;
+
+            case WingModes.HOLD:
+                //Rollers shouldn't have to move, arms should be slightly angled in. [\O/]
+                Util.log("WingController", "Mode: HOLD");
+
+                robot.hw.backWing.rollersOff();
+                robot.hw.frontWing.rollersOff();
+
+                robot.hw.backWing.setTargetAngle(-20); // ALso guesstimated
+                robot.hw.frontWing.setTargetAngle(-20); //halp
+                break;
+
+            case WingModes.KISS:
+                //Rollers should push ball out, back wing should punt ball out, front roller up.
+                Util.log("WingController", "Mode: KISS");
+
+                robot.hw.frontWing.rollersOff();
+                robot.hw.backWing.rollersUp();
+                //oh baby i didn't code this yet
+                break;
+
+            case WingModes.PICKUP:
+                //Front rollers down, back off.
+                Util.log("WingController", "Mode: PICKUP");
+
+                robot.hw.frontWing.rollersDown();
+                robot.hw.backWing.rollersOff();
+
+                robot.hw.backWing.setTargetAngle(0);
+                robot.hw.frontWing.setTargetAngle(85);
+                break;
+
+            case WingModes.DROP:
+                //Prepare to kick
+                Util.log("WingController", "Mode: DROP");
+
+                robot.hw.frontWing.rollersUp();
+                robot.hw.backWing.rollersOff();
+
+                robot.hw.frontWing.setTargetAngle(180);
+                robot.hw.backWing.setTargetAngle(120);
+
+                timer = 30;
+                break;
+
+            case WingModes.DROP_KICK:
+                //KICK THE SHIT OUT OF IT
+                Util.log("WingController", "Mode: DROP");
+
+                robot.hw.backWing.setTargetAngle(300);
+
+                timer = 30;
+                break;
+
+            case WingModes.FLUSH:
+                //Rollers shouldn't have to move, arms should be flushish off i think. [| |]
+
+                robot.hw.backWing.disable();
+                robot.hw.frontWing.disable();
+                break;
+
+            case WingModes.SHOULD_FLUSH:
+                if (Robot.getInstance().hw.sonar.ballDetected()) { // if ya see dem balls
+                    this.setState(WingModes.HOLD); // Grab dem balls
                     break;
+                }
 
-                case WingModes.HOLD:
-                    //Rollers shouldn't have to move, arms should be slightly angled in. [\O/]
-                    Util.log("WingController", "Mode: HOLD");
+                Util.log("WingController", "Mode: SHOULD_FLUSH");
+                //redy 4 teh flush
+                robot.hw.backWing.rollersOff();
+                robot.hw.frontWing.rollersOff();
 
-                    robot.hw.backWing.rollersOff();
-                    robot.hw.frontWing.rollersOff();
+                robot.hw.backWing.setTargetAngle(0);
+                robot.hw.frontWing.setTargetAngle(0);
 
-                    robot.hw.backWing.setTargetAngle(-20); // ALso guesstimated
-                    robot.hw.frontWing.setTargetAngle(-20); //halp
-                    break;
-
-                case WingModes.KISS:
-                    //Rollers should push ball out, back wing should punt ball out, front roller up.
-                    Util.log("WingController", "Mode: KISS");
-
-                    robot.hw.frontWing.rollersOff();
-                    robot.hw.backWing.rollersUp();
-                    //oh baby i didn't code this yet
-                    break;
-
-                case WingModes.PICKUP:
-                    //Front rollers down, back off.
-                    Util.log("WingController", "Mode: PICKUP");
-
-                    robot.hw.frontWing.rollersDown();
-                    robot.hw.backWing.rollersOff();
-
-                    robot.hw.backWing.setTargetAngle(0);
-                    robot.hw.frontWing.setTargetAngle(85);
-                    break;
-
-                case WingModes.DROP:
-                    //Prepare to kick
-                    Util.log("WingController", "Mode: DROP");
-
-                    robot.hw.frontWing.rollersUp();
-                    robot.hw.backWing.rollersOff();
-
-                    robot.hw.frontWing.setTargetAngle(180);
-                    robot.hw.backWing.setTargetAngle(120);
-
-                    timer = 30;
-                    break;
-
-                case WingModes.DROP_KICK:
-                    //KICK THE SHIT OUT OF IT
-                    Util.log("WingController", "Mode: DROP");
-
-                    robot.hw.backWing.setTargetAngle(300);
-
-                    timer = 30;
-                    break;
-
-                case WingModes.FLUSH:
-                    //Rollers shouldn't have to move, arms should be flushish off i think. [| |]
-
-                    robot.hw.backWing.disable();
-                    robot.hw.frontWing.disable();
-                    break;
-
-                case WingModes.SHOULD_FLUSH:
-                    Util.log("WingController", "Mode: SHOULD_FLUSH");
-                    //redy 4 teh flush
-                    robot.hw.backWing.rollersOff();
-                    robot.hw.frontWing.rollersOff();
-
-                    robot.hw.backWing.setTargetAngle(0);
-                    robot.hw.frontWing.setTargetAngle(0);
-
-                    timer = 30;
-                    break;
-            }
+                timer = 30;
+                break;
         }
     }
 
--- src/ca/warp7/frc2014/util/WingModes.java
+++ src/ca/warp7/frc2014/util/WingModes.java
@@ -4,7 +4,20 @@ public class WingModes {
     protected WingModes() {
         //No.
     }
+    /*
+        A poem.
 
+        Dear FIRST.
+        Why must we use 1.3
+        It's crappy
+        It's old
+        It makes my tear my hair out
+        I can't use enums
+        I can't use HashMaps
+        I can't use @Override
+        It makes my cry at night
+        I can't take this anymore
+     */
     public static final int HOLD = 0;
     public static final int FLUSH = 1;
     public static final int PICKUP = 2;
